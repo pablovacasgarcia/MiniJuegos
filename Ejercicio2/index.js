56ca14b15a95f1c3112fbe2e662e003ca1723a62
Pablo Vacas <pablovacasgarcia@gmail.com>
2023-11-12 17:45:29 +0100

    Adaptacion Ahoracdo para movil

--- Ejercicio2/index.js
+++ Ejercicio2/index.js
@@ -1,5 +1,5 @@
 window.onload=()=>{
-    palabras=["pollo", "jugar", "ahorcado", "vaca"]
+    palabras = ["pollo", "jugar", "ahorcado", "vaca", "gato", "casa", "perro", "pelota", "luz", "mesa", "silla", "rojo", "azul", "verde", "amarillo", "manzana", "computadora", "musica", "libro", "caminar", "naranja", "jardin", "hoja", "teclado", "nube", "sol", "cielo", "montaña", "rio", "playa", "isla", "avion", "autobus", "tren", "coche", "bicicleta", "reloj", "tiempo", "papel", "foto", "guitarra", "piano", "juego", "escuela", "estudio", "arte", "foto", "tarjeta", "fruta", "leche", "pan", "queso", "fiesta", "fiesta", "alegre", "feliz", "triste", "llorar", "reir", "saltar", "correr", "nadar", "dormir", "soñar", "comer", "beber", "hablar", "escuchar", "leer", "escribir", "aprender", "enseñar", "viajar", "descubrir", "crear", "deporte", "jugar", "amigo", "amor", "familia", "hogar", "naturaleza", "ciencia", "historia", "musica", "arte", "poesia", "felicidad", "paz"]
 
     palabraDiv=document.getElementById("palabra");
     palabraSpan=document.getElementById("palabraSpan");
